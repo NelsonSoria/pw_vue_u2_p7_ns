e724f85d7504b4b8f119cae1ca3d36540b411d13
Nelson Soria <nelson-soria@hotmail.com>
2024-12-18 19:44:47 -0500

    Taller17

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="WELCOM Nelson Soria"/>
+  <Contador/>
+   <Contador/>
+   <Contador/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Contador from './components/Contador.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Contador
   }
 }
 </script>
---
+++ src/components/Contador.vue
@@ -0,0 +1,58 @@
+
+<template>
+  <h2>{{ titulo }}</h2>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+
+  <div>
+    <button v-on:click="incrementar()">+1</button>
+    <button v-on:click="decrementar()">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero: 10,
+      titulo: "Contador",
+    };
+  },
+  methods: {
+    calcularCuadrado() {
+      console.log("Entro a Calcular");
+      return this.numero * this.numero;
+    },
+    incrementar() {
+      this.numero++;
+    },
+    decrementar() {
+      this.numero--;
+    },
+  },
+  computed: {
+    calcularCuadradoComputed() {
+      console.log("Entro a Calcular computado");
+      return this.numero * this.numero;
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  background: #64b687;
+  border-radius: 5px;
+  margin: 0px 10px;
+  border: 1px solid white;
+  color:white;
+  cursor: pointer;
+  padding: 5px 20px;
+}
+button :hover {
+ background: #5aa67b;
+}
+</style>
